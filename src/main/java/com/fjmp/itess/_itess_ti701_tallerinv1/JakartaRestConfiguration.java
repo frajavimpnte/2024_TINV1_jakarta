e9125c1581ab8d13ef3e89c78a5957eb3356c868
frajavimopu <frajavimopu@gmail.com>
2024-10-21 17:53:26 -0500

    2024 Taller Invesgigación 1, Jakarta test

---
+++ src/main/java/com/fjmp/itess/_itess_ti701_tallerinv1/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.fjmp.itess._itess_ti701_tallerinv1;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/fjmp/itess/_itess_ti701_tallerinv1/resources/JakartaEE9Resource.java
@@ -0,0 +1,20 @@
+package com.fjmp.itess._itess_ti701_tallerinv1.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee9")
+public class JakartaEE9Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
